5c2adc768c6d782ca0019dcf1d2631a0376cc19f
Larafdezmorena <larafdezmorena@gmail.com>
2024-01-09 18:41:31 +0100

    add query-selectors

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('>> Ready :)');
+// QUERY-SELECTOR
+
+const searchInput = document.querySelector('.js__input');
+const searchBtn = document.querySelector('.js__search-btn');
+const favorites = document.querySelector('.js__favorites-list');
+const character = document.querySelector('.js__character-list');
+
+
+// VARIABLES DE DATOS
+
+// EVENTOS
+
+// CÓDIGO CUANDO INICIA LA PÁGINA
+
+//window.addEventListener('load', (event) =>{
+
+//});
