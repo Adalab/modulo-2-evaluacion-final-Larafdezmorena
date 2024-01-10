d9c2e2ac91440c8f92e8d1612e535bf4c2618679
Larafdezmorena <larafdezmorena@gmail.com>
2024-01-10 07:56:18 +0100

    add handle event

--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,19 @@ const favorites = document.querySelector('.js__favorites-list');
 const charactersResultUl = document.querySelector('.js__character-list');
 
 
+
+
+
+
 // VARIABLES GLOBALES
 let charactersData = [];
-
-
-// EVENTOS
 
 
 //FUNCIONES
 
 function renderOne(characterData) {
     charactersResultUl.innerHTML += `
-    <li class="cards__list">
+    <li class="cards__list js__characters-li">
         <h3>${characterData.name}</h3>
         <img src="${characterData.imageUrl}"/>
         </li>
@@ -31,6 +32,20 @@ function renderAll () {
     }
 }
 
+const allCharactersLi = document.querySelectorAll('.js__characters-li');
+
+for( const characterLi of allCharactersLi) {
+    characterLi.addEventListener('click', handleClickResult);
+}
+
+// FUNCIONES DE EVENTOS (HANDLER)
+
+function handleClickResult(event) {
+    const clickedLi = event.currentTarget;
+    clickedLi.classList.toggle('favorites')
+}
+
+// EVENTOS
 // CÓDIGO CUANDO CARGA LA PÁGINA
 
 renderAll();
