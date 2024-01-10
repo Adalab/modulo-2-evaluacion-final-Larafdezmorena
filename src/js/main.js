88ad0ba1f3823443bde171b21d3fde4cc4b59bfe
Larafdezmorena <larafdezmorena@gmail.com>
2024-01-10 17:50:07 +0100

    select favorites

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,9 @@
 
 // QUERY-SELECTOR
 const searchInput = document.querySelector('.js__input');
-const searchBtn = document.querySelector('.js__search-btn');
+const form = document.querySelector('.js__form');
 const favoritesUl = document.querySelector('.js__favoritesUl');
 const charactersResultUl = document.querySelector('.js__character-list');
-
-
-
-
 
 
 // VARIABLES DE DATOS
@@ -21,65 +17,99 @@ let favoritesData =[];
 
 function renderOne(characterData) {
     charactersResultUl.innerHTML += `
-    <li class="cards__list js__characters-li" data-id="${characterData.id}">
+        <li class="cards__list js__characters-li" data-id="${characterData.id}">
         
-        <img class="img__card" src="${characterData.imageUrl}"/>    
-        <h3 class="name">${characterData.name}</h3>
+            <img class="img__card" src="${characterData.imageUrl}"/>    
+            <h3 class="name">${characterData.name}</h3>
         
         </li>
     `;
+
 }
 
 function renderAll () { 
+    charactersResultUl.innerHTML = '';
+    
     for ( const eachCharacter of charactersData ) {
         renderOne ( eachCharacter );
     }
-}
 
-const allCharactersLi = document.querySelectorAll('.js__characters-li');
+    const allCharactersLi = document.querySelectorAll('.js__characters-li');
 
-for( const characterLi of allCharactersLi) {
+    for( const characterLi of allCharactersLi) {
     characterLi.addEventListener('click', handleClickResult);
+    }
 }
 
 // FUNCIONES DE EVENTOS (HANDLER)
 
 function handleClickResult(event) {
     console.log('click');
-    
     const clickedLi = event.currentTarget;
     
     clickedLi.classList.toggle('favorites');
 
     clickedLi.dataset.id;
 
-    const selectedCharacterOb = charactersData.find( oneCharacter => oneCharacter.id === clickedLi.dataset.id );
-
-    favoritesUl.innerHTML += `
-    <li class="cards__list js__characters-li">
-        
-        <img class="img__card" src="${selectedCharacterOb.imageUrl}"/>    
-        <h3 class="name">${selectedCharacterOb.name}</h3>
+    const selectedCharacterOb = charactersData.find( oneCharacter => oneCharacter._id === parseIntclickedLi.dataset.id );
     
-    </li>
-`;
+    if (selectedCharacterOb) {
+        const imageUrl = selectedCharacterOb.imageUrl || "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
+    
+        favoritesUl.innerHTML += `
+            <li class="cards__list js__characters-li">
+        
+                <img class="img__card" src="${selectedCharacterOb.imageUrl}"/>    
+                <h3 class="name">${selectedCharacterOb.name}</h3>
+    
+            </li>
+        `;
+    }
+}
+
+function createFav (character) {
+
+    const cardFav = document.createElement('div');
+
+    cardFav.classList.add('cardFav', 'js_card');
+
+    cardFav.setAttribute('id', charactersData.id);
+
+
 }
 
 
 
 // EVENTOS
+
+form.addEventListener('submit', (event)=> {
+
+    event.preventDefault();
+    fetch(`//api.disneyapi.dev/character?name=${searchInput.value}`)
+        .then(response => response.json())
+        .then(data=> {
+            charactersData = data.character;
+
+            renderAll();
+    })
+    .catch(error => console.error('Error al obtener datos:', error));
+}) ; 
+   
+
+
+
 // CÓDIGO CUANDO CARGA LA PÁGINA
 
 renderAll();
 
-fetch ('//api.disneyapi.dev/character?pageSize=50')
-.then( response => response.json())
-.then( data => {
+fetch (`//api.disneyapi.dev/character?pageSize=50`)
+    .then( response => response.json())
+    .then( data => {
 
 
-    charactersData = data.data;
+        charactersData = data.data;
 
-    renderAll();
+        renderAll();
 });
 
 
