51d2f031b770ebbaaf13dcb09eda5eac2c16c54f
Larafdezmorena <larafdezmorena@gmail.com>
2024-01-10 07:30:29 +0100

    add fecht

--- src/js/main.js
+++ src/js/main.js
@@ -8,62 +8,8 @@ const charactersResultUl = document.querySelector('.js__character-list');
 
 
 // VARIABLES GLOBALES
-let charactersData = [{
-    "_id": 112,
-    "films": [
-    "Hercules (film)"
-    ],
-    "shortFilms": [],
-    "tvShows": [
-    "Hercules (TV series)"
-    ],
-    "videoGames": [
-    "Kingdom Hearts III"
-    ],
-    "parkAttractions": [],
-    "allies": [],
-    "enemies": [],
-    "sourceUrl": "https://disney.fandom.com/wiki/Achilles_(Hercules)",
-    "name": "Achilles",
-    "imageUrl": "https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png",
-    "createdAt": "2021-04-12T01:31:30.547Z",
-    "updatedAt": "2021-12-20T20:39:18.033Z",
-    "url": "https://api.disneyapi.dev/characters/112",
-    "__v": 0
-    },
-    {
-        "_id": 18,
-        "films": [
-        "The Fox and the Hound",
-        "The Fox and the Hound 2"
-        ],
-        "shortFilms": [],
-        "tvShows": [],
-        "videoGames": [],
-        "parkAttractions": [],
-        "allies": [],
-        "enemies": [],
-        "sourceUrl": "https://disney.fandom.com/wiki/Abigail_the_Cow",
-        "name": "Abigail the Cow",
-        "imageUrl": "https://static.wikia.nocookie.net/disney/images/0/05/Fox-disneyscreencaps_com-901.jpg",
-        "createdAt": "2021-04-12T01:26:03.413Z",
-        "updatedAt": "2021-12-20T20:39:18.032Z",
-        "url": "https://api.disneyapi.dev/characters/18",
-        "__v": 0
-        },
+let charactersData = [];
 
-
-];
-//let disneyFavorites = JSON.parse(localStorage.getItem("disneyFavorites")) || [];
-
-
-// CÓDIGO CUANDO INICIA LA PÁGINA
-
-//se deben de mostrar los personajes
-
-//window.addEventListener('load', (event) =>{
-//ev.preventDefault()
-//});
 
 // EVENTOS
 
@@ -79,12 +25,24 @@ function renderOne(characterData) {
     `;
 }
 
-// CÓDIGO CUANDO CARGA LA PÁGINA
-
-
-for ( const eachCharacter of charactersData ) {
-    renderOne ( eachCharacter );
+function renderAll () { 
+    for ( const eachCharacter of charactersData ) {
+        renderOne ( eachCharacter );
+    }
 }
 
+// CÓDIGO CUANDO CARGA LA PÁGINA
+
+renderAll();
+
+fetch ('//api.disneyapi.dev/character?pageSize=50')
+.then( response => response.json())
+.then( data => {
+    console.log(data.data);
+
+    charactersData = data.data;
+
+    renderAll();
+});
 
 
